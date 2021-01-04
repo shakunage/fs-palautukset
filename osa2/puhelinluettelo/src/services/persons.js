4cc8cd0a6e81f01a37246d5fbe32ca9b9c40fdea
shakunage <veikka.junttila1@gmail.com>
2021-01-04 21:11:31 +0200

    varoitus poistettu

--- osa2/puhelinluettelo/src/services/persons.js
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'http://localhost:3001/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
@@ -21,4 +21,10 @@ const update = (id, newObject) => {
   return request.then(response => response.data)
 }
 
-export default { getAll, create, remove, update }
+const features = {
+  getAll,
+  create, 
+  remove, 
+  update
+}
+export default features
